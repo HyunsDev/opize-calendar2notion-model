9eb6fe5507bab1c27fbd2d6eb3152f96530a428a
혀느현스 <hyuns@hyuns.dev>
2024-01-08 15:17:00 +0900

    feat: add postUserCalendarRename

--- src/endpoint/user/calendar/index.ts
+++ src/endpoint/user/calendar/index.ts
@@ -6,7 +6,10 @@ export type PostUserCalendarParameter = {
     googleCalendarId: string;
 };
 export type PostUserCalendarResponse = {};
-export const postUserCalendar: Endpoint<PostUserCalendarParameter, PostUserCalendarResponse> = {
+export const postUserCalendar: Endpoint<
+    PostUserCalendarParameter,
+    PostUserCalendarResponse
+> = {
     method: 'POST',
     path: (e) => `/users/${e.userId}/calendar`,
     bodyParams: ['googleCalendarId'],
@@ -22,9 +25,31 @@ export type DeleteUserCalendarParameter = {
 export type DeleteUserCalendarResponse = {
     code?: 'user_is_work';
 };
-export const deleteUserCalendar: Endpoint<DeleteUserCalendarParameter, DeleteUserCalendarResponse> = {
+export const deleteUserCalendar: Endpoint<
+    DeleteUserCalendarParameter,
+    DeleteUserCalendarResponse
+> = {
     method: 'DELETE',
     path: (e) => `/users/${e.userId}/calendar/${e.calendarId}`,
+    bodyParams: [],
+    pathParams: ['userId', 'calendarId'],
+    queryParams: [],
+};
+
+// POST /user/:userId/calendar/:calendarId/rename
+export type PostUserCalendarRenameParameter = {
+    userId: 'me' | number;
+    calendarId: string;
+};
+export type PostUserCalendarRenameResponse = {
+    code: 'already_same_name' | 'calendar_name_changed';
+};
+export const postUserCalendarRename: Endpoint<
+    PostUserCalendarRenameParameter,
+    PostUserCalendarRenameResponse
+> = {
+    method: 'POST',
+    path: (e) => `/users/${e.userId}/calendar/${e.calendarId}/rename`,
     bodyParams: [],
     pathParams: ['userId', 'calendarId'],
     queryParams: [],
